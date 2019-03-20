17a0f24c7c49add1828b1b308176b40502162a9b
Pritam Kumar <pk.rajece2010@gmail.com>
2019-03-21 01:45:13 +0530

    Initial commit

---
+++ precache-manifest.79fb4cb0d12a48bd2fdf53e12f53ce94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d61251936556fc3c7414",
+    "url": "/static/js/main.e83e7934.chunk.js"
+  },
+  {
+    "revision": "e053a1da17cbfeebd8e3",
+    "url": "/static/js/2.9176d4f9.chunk.js"
+  },
+  {
+    "revision": "d61251936556fc3c7414",
+    "url": "/static/css/main.ba8b3502.chunk.css"
+  },
+  {
+    "revision": "a3cc8588b7a4a661d2defe2812b0b177",
+    "url": "/index.html"
+  }
+];
